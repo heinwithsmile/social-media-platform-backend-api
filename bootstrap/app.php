d960654362d097dd637eb8c92fec061926ed6525
heinhtetaung <heinhtetaung7715@gmail.com>
2025-09-25 16:22:53 +0630

    cors configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -12,7 +12,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        $middleware->trustProxies(at: '*');
+        $middleware->trustHosts(at: ['*']);
+        $middleware->web(\Illuminate\Http\Middleware\HandleCors::class);
+        $middleware->api(\Illuminate\Http\Middleware\HandleCors::class);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         //
--- routes/api.php
+++ routes/api.php
@@ -12,18 +12,18 @@ use App\Http\Controllers\HomeController;
 |--------------------------------------------------------------------------
 */
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+Route::post('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api')->name('logout');
 
 // Profile
-Route::get('/profile', [UserController::class, 'profile'])->middleware('auth:api');
+Route::get('/profile', [UserController::class, 'profile'])->middleware('auth:api')->name('profile');
 
 // Posts
 Route::apiResource('posts', PostController::class)->middleware('auth:api');
 
 // NewFeed
-Route::get('/posts', [HomeController::class, 'getAllPosts'])->middleware('auth:api');
-Route::get('/posts/{postId}/comments', [HomeController::class, 'getCommentsByPostId'])->middleware('auth:api');
-Route::get('/posts/{postId}/reactions', [HomeController::class, 'getReactionsByPostId'])->middleware('auth:api');
+Route::get('/posts', [HomeController::class, 'getAllPosts'])->middleware('auth:api')->name('posts.index');
+Route::get('/posts/{postId}/comments', [HomeController::class, 'getCommentsByPostId'])->middleware('auth:api')->name('posts.comments');
+Route::get('/posts/{postId}/reactions', [HomeController::class, 'getReactionsByPostId'])->middleware('auth:api')->name('posts.reactions');
 
